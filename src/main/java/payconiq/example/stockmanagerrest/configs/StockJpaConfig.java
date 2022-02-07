6501cd2dac7e8d3e23394b41aeac3a92b56617f0
bird <mostafa_shaeri_tj@yahoo.com>
2022-02-07 13:14:12 +0330

    Unnecessary import removed

--- src/main/java/payconiq/example/stockmanagerrest/configs/StockJpaConfig.java
+++ src/main/java/payconiq/example/stockmanagerrest/configs/StockJpaConfig.java
@@ -10,11 +10,8 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "payconiq.example.stockmanagerrest.repositories")//, entityManagerFactoryRef="emf"
