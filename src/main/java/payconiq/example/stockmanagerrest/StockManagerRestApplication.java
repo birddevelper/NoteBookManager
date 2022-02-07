7998e2991a74eba18c16b7fdc719f0bee633c2cd
bird <mostafa_shaeri_tj@yahoo.com>
2022-02-07 19:56:53 +0330

    unused import removed

--- src/main/java/payconiq/example/stockmanagerrest/StockManagerRestApplication.java
+++ src/main/java/payconiq/example/stockmanagerrest/StockManagerRestApplication.java
@@ -1,20 +1,21 @@
 package payconiq.example.stockmanagerrest;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
-import payconiq.example.stockmanagerrest.entities.Stock;
-import payconiq.example.stockmanagerrest.repositories.StockRepository;
+
 
 @SpringBootApplication
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class StockManagerRestApplication {
 
+
     public static void main(String[] args) {
 
         SpringApplication.run(StockManagerRestApplication.class, args);
 
+
     }
+
+
 
 }
--- src/test/java/payconiq/example/stockmanagerrest/StockManagerRestApplicationTests.java
+++ src/test/java/payconiq/example/stockmanagerrest/StockManagerRestApplicationTests.java
@@ -3,7 +3,7 @@ package payconiq.example.stockmanagerrest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest()
 public class StockManagerRestApplicationTests {
 
     @Test
